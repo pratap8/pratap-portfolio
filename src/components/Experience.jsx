81d14df780973195b18c420565b4f0c277a80154
pratap8 <phool8790@gmail.com>
2025-10-17 00:48:42 +0530

    Initial portfolio commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -17,7 +17,19 @@ const Experience = () => {
           <li>Collaborated with cross-functional teams in agile sprints for smooth deliverables.</li>
         </ul>
       </div>
+      <div className="experience-card">
+        <h3>Software Engineer – LTIMindtree</h3>
+        <p className="duration">March 2022 – June 2025 | Chennai (Hybrid)</p>
+        <ul>
+          <li>Developed executable code and optimized SQL queries, improving performance by 30%.</li>
+          <li>Enhanced UX using Chart.js with dynamic visualizations for personalized user data.</li>
+          <li>Built RESTful APIs using Spring Boot ensuring scalable backend systems.</li>
+          <li>Performed code analysis with SonarQube & JUnit (90% coverage).</li>
+          <li>Collaborated with cross-functional teams in agile sprints for smooth deliverables.</li>
+        </ul>
+      </div>
     </section>
+    
   );
 };
 
